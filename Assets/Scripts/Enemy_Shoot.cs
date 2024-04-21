38a301c40fbe521f834b70d21401ffa90441fb86
Blenisj <148898744+Blenisj@users.noreply.github.com>
2024-04-20 22:50:30 -0400

    added more enemys and fixed AI

--- Assets/Scripts/Enemy_Shoot.cs
+++ Assets/Scripts/Enemy_Shoot.cs
@@ -9,7 +9,8 @@ public class Enemy_Shoot : MonoBehaviour
     public float minDamage;
     public float maxDamage;
     public float projectileForce;
-    public float cooldown;
+    public float minCooldown;
+    public float maxCooldown;
     public float spawnDistance = 1.0f;
 
     void Start()
@@ -19,8 +20,8 @@ public class Enemy_Shoot : MonoBehaviour
 
     IEnumerator ShootPlayer()
     {
-
-        yield return new WaitForSeconds(cooldown);
+        float randomCooldown = Random.Range(minCooldown, maxCooldown);
+        yield return new WaitForSeconds(randomCooldown);
         if (player != null)
         {
             GameObject shoot = Instantiate(Projectile, transform.position, Quaternion.identity);
@@ -33,7 +34,7 @@ public class Enemy_Shoot : MonoBehaviour
         }
         IEnumerator ShootPlayer()
         {
-            yield return new WaitForSeconds(cooldown);
+            yield return new WaitForSeconds(randomCooldown);
             if (player != null)
             {
                 Vector2 mypos = transform.position;
